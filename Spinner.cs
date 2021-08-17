1ab980ceb9cdc9ebf4b241659115c01d493715d6
iioaia <52546326+iioaia@users.noreply.github.com>
2021-08-17 13:35:35 -0400

    Update Spinner.cs
    Added Comments

--- Spinner.cs
+++ Spinner.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {   
+
+//  When attached to a game object, in the Inspector you can determine which angle and at what speed the object will rotate.
+
     [SerializeField] float xAngle = 0.0f;
     [SerializeField] float yAngle = 0.0f;
     [SerializeField] float zAngle = 0.0f;
 
 
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(xAngle,yAngle,zAngle);
